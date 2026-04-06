974091b973e5e24e7dc642e904b4bc54b8035b5f
SRIRAM <wavene20@gmail.com>
2026-04-06 20:35:11 +0530

    04/06/2026 - waits

--- src/main/java/selenium/NavigationCMD.java
+++ src/main/java/selenium/NavigationCMD.java
@@ -39,6 +39,8 @@ public class NavigationCMD {
 		
 		driver.quit();
 		
+
+		
 		
 	}
 
--- src/main/java/web_elements/ById.java
+++ src/main/java/web_elements/ById.java
@@ -4,24 +4,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class ById {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException 
 	{
 		// TODO Auto-generated method stub
 		
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new EdgeDriver();
 		
 		driver.manage().window().maximize();
 		
 		driver.get("https://www.google.com");
 		
-		WebElement search = driver.findElement(By.id("APjFqb"));
+		WebElement search = driver.findElement(By.name("q"));
 		
-		search.sendKeys("vijay");
+		search.sendKeys("chat gpt");
 		
 		search.submit();
+		
+		Thread.sleep(10000);
 		
 		System.out.println(driver.getTitle());
 		
