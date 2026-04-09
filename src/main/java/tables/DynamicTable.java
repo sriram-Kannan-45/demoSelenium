1ed569fcf67e0d6d92e1f5145e17a073126f68f7
SRIRAM <wavene20@gmail.com>
2026-04-09 21:15:15 +0530

    js selenium

--- src/main/java/tables/DynamicTable.java
+++ src/main/java/tables/DynamicTable.java
@@ -1,60 +1,54 @@
 package tables;
+//2k22aids46@kiot.ac.in sriram123@
 
+import java.time.Duration;
 import java.util.List;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class DynamicTable 
-{
 
-	public static void main(String[] args) 
-	
+public class DynamicTable {
+
+	public static void main(String[] args)
+
 	{
 		// TODO Auto-generated method stub
-		
 		WebDriver driver = new ChromeDriver();
-		
 		driver.manage().window().maximize();
-		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://thinking-tester-contact-list.herokuapp.com/");
+		WebElement user = driver.findElement(By.cssSelector("input#email"));
+		user.sendKeys("2k22aids46@kiot.ac.in");
+
+		WebElement user1 = driver.findElement(By.cssSelector("input#password"));
+		user1.sendKeys("sriram123@");
+
+		WebElement submit = driver.findElement(By.cssSelector("button#submit"));
+		submit.click();
+
+		List<WebElement> tabler = driver.findElements(By.xpath("//table[@id='myTable']//tr//td[2]"));
+		int r = tabler.size();
+		System.out.println("Total contacts: " + r);
 		
-		WebElement email = driver.findElement(By.xpath("//input[@id=\"email\"]"));
-		email.sendKeys("2k22aids46@kiot.ac.in");
-		
-		WebElement pass = driver.findElement(By.xpath("//input[@id=\"password\"]"));
-		pass.sendKeys("sriram123@");
-		
-		driver.findElement(By.xpath("//button[@id=\"submit\"]")).click();
-		
-		
-		
-		List<WebElement> col_datas = driver.findElements(By.xpath("//table[@id=\"myTable\"]//th"));
-		
-		int col = col_datas.size();
-		
-		List<WebElement> row_datas =  driver.findElements(By.xpath("//table[@id=\"myTable\"]//tr"));
-		
-		int row = row_datas.size();
-		
-		WebElement data ;
-		
-		for(int i = 1 ; i<=row ; i++)
-		{
-			for (int j = 1 ; j<=col ; j++)
-			{
-				if (i==1)
-				{
-					data = driver.findElement(By.xpath("//table[@id=\"myTable\"]//thead/tr/th["+j+"]"));
-					System.out.print(data.getText()+" ");
-				}
-				
-				else
-				{
-					data = driver.findElement(By.xpath("//table[@id=\"myTable\"]//tbody//tr["+(i-1)+"]/td["+j+"]"));
-					System.out.print(data.getText()+" ");
-				}
-			}
-			System.out.println();
+		for (WebElement i : tabler) {
+			System.out.println(i.getText());
 		}
-}
+		int i = 1;
+		for (WebElement j : tabler) 
+		{
+			if (j.getText().equals("ramanan v") || j.getText().equals("aravinth kumar")) 
+			{
+				List<WebElement> actual = driver.findElements(By.xpath("//table[@id='myTable']//tr[" + i + "]"));
+				for (WebElement row : actual) 
+				{
+					System.out.println(row.getText());
+				}
+
+			}
+			i++;
+		}
+	}
+
 }
